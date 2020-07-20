b3daacc1f39094c82a41b1a79721240d98ce723c
Solomon Gichuru <solomonggichuru@gmail.com>
2020-07-21 01:37:14 +0300

    Add index.js route

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 // Import express package
 const express = require('express');
-const upload = require('./upload');
+
+let indexRouter = require('./routes/index');
 
 // Initialize express
 const app = express();
@@ -8,30 +9,17 @@ const app = express();
 // Set up a view engine
 app.set('view engine', 'ejs');
 
+
 // Set a static folder
 app.use(express.static('public'));
 
-app.get('/', (req,res)=>{
-   res.render('index');
-})
-
-// route to handle image upload
-app.post('/upload', (req,res)=>{
-   upload(req, res, (err)=>{
-       if (err){
-           console.log(err)
-           res.render('index', {msg: err})
-       }else{
-           res.render('index', {file: 'images/' + req.file.filename})
-       }
-   })
-})
+// Define the index router
+app.use('/', indexRouter);
 
 
 // Define the port number
-const PORT = 5001;
+const PORT = 5000;
 
-
-app.listen(PORT, function(){
+app.listen(PORT, ()=>{
    console.log(`Server is listening on port ${PORT}`)
-})
+});
