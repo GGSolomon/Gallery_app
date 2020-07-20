147cecca7843d1573d6f0f9ff45f22d883f831e4
Solomon Gichuru <solomonggichuru@gmail.com>
2020-07-21 00:57:34 +0300

    Add upload file

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
-// Import express framework
+// Import express package
 const express = require('express');
+const upload = require('./upload');
 
 // Initialize express
 const app = express();
@@ -12,6 +13,18 @@ app.use(express.static('public'));
 
 app.get('/', (req,res)=>{
    res.render('index');
+})
+
+// route to handle image upload
+app.post('/upload', (req,res)=>{
+   upload(req,res, (err)=>{
+       if (err){
+           console.log(err)
+       }else{
+           console.log(req.file)
+           res.send('test');
+       }
+   })
 })
 
 // Define the port number
